b43103be08fb9885947512fd6395f3f2a1e11b79
fidadoma <filip.dechterenko@gmail.com>
2020-06-02 11:25:49 +0200

    download data from osf only if specified

--- render_analysis.R
+++ render_analysis.R
@@ -4,11 +4,11 @@
 analysis_type <- "main"
 
 rmarkdown::render("Weissman-replication-raw-processed.Rmd", params = list(dataset = "main"))
-rmarkdown::render("Weissman-replication-analysis.Rmd", params = list(dataset = "main"))
+rmarkdown::render("Weissman-replication-analysis.Rmd", params = list(dataset = "main",download_from_osf = "no"))
 
 # withouht first trial
 
 analysis_type <- "no_fst"
 
 rmarkdown::render("Weissman-replication-raw-processed.Rmd", params = list(dataset = "main"))
-rmarkdown::render("Weissman-replication-analysis.Rmd", params = list(dataset = "main"))
+rmarkdown::render("Weissman-replication-analysis.Rmd", params = list(dataset = "main",download_from_osf = "no"))
