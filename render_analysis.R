3b045a6c216d4cb640ebb5ac0473ae637d067e6b
fidadoma <filip.dechterenko@gmail.com>
2020-06-02 11:32:24 +0200

    minor changes

--- render_analysis.R
+++ render_analysis.R
@@ -1,14 +1,16 @@
 
+library(rmarkdown)
+
 # Main analysis -----------------------------------------------------------
 
 analysis_type <- "main"
 
-rmarkdown::render("Weissman-replication-raw-processed.Rmd", params = list(dataset = "main"))
-rmarkdown::render("Weissman-replication-analysis.Rmd", params = list(dataset = "main",download_from_osf = "no"))
+render("Weissman-replication-raw-processed.Rmd", params = list(dataset = analysis_type))
+render("Weissman-replication-analysis.Rmd", params = list(dataset = analysis_type,download_from_osf = "no"))
 
-# withouht first trial
+# without first trial  -----------------------------------------------------------
 
 analysis_type <- "no_fst"
 
-rmarkdown::render("Weissman-replication-raw-processed.Rmd", params = list(dataset = "main"))
-rmarkdown::render("Weissman-replication-analysis.Rmd", params = list(dataset = "main",download_from_osf = "no"))
+render("Weissman-replication-raw-processed.Rmd", params = list(dataset = analysis_type))
+render("Weissman-replication-analysis.Rmd", params = list(dataset = analysis_type, download_from_osf = "no"))
