e27ad7e92224935623270311390894d2f8742b5a
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-06-12 11:03:18 +0100

    Multiverse analysis script
    Added a multiverse analysis script to run the processing and analysis scripts with various rules for inclusion and exclusion.
    Tweaked analysis and processing scripts to only require/use OSF stuff if downloading data from the OSF.

---
+++ multiverse.R
@@ -0,0 +1,35 @@
+
+library(rmarkdown)
+
+download_from_osf = "no"
+datasets <- c("main", "no_fst", "no_scnd", "both")
+combined <- list()
+
+for (d in datasets) {
+  p$dataset <- d
+  tmp <- tempfile()
+  render("Weissman-replication-raw-processed.Rmd", 
+         params = list(dataset = d, download_from_osf = download_from_osf), 
+         output_file = tmp)
+  # Never download Processed data from the OSF because we just processed a fresh copy!
+  render("Weissman-replication-analysis.Rmd", 
+         params = list(dataset = d, download_from_osf = "no"), 
+         output_file = tmp)
+  # Save each of the analysis dataframes into a dataframe with the dataset as a column
+  for (x in ls()) {
+    if (typeof(get(x)) == "list" && 
+        !(x %in% c("combined", "p", "params", "datasets"))) {
+      df <- get(x) 
+      df$dataset <- d
+      combined[[x]] <- bind_rows(combined[[x]], df)
+    }
+  }
+}
+
+# Clean the environment
+rm(list = ls()[!ls() %in% "combined"])
+
+for (x in names(combined)) {
+  print(x)
+  print(combined[[x]])
+}
